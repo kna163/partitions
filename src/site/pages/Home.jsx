332f9ccd72d1356f753baf46d53e270103a12cf8
kna163 <knae163@gmail.com>
2025-08-30 19:47:33 -0700

    small home page update

--- src/site/pages/Home.jsx
+++ src/site/pages/Home.jsx
@@ -5,7 +5,10 @@ export default function Home() {
     <>
       <Navbar />
   
-      <h1>Learn about things on the home page!</h1>;
+      <h1>Home</h1>
+      <p> This project is currently a work in progress! Many years ago, I had worked with integer partitions for a bit, and had come across some interesting ideas. In the interest of sharing some of these facts and to develop some of the ideas from back then into more interactive and accessible forms, this project came about.
+      </p>
+      <p>Currently, the text-based calculator works which provides an interface to the javascript port of some of the core funcitons, but in the future, slides explaining the concepts will be added, as well as an interactive visual calculator! However, doing this will take a bit of time as I familiarize myself with React and other front-end stuff.</p>
     </>
   )
 }
